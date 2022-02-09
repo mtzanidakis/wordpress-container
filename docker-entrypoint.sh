326a2566f18749e248829883e9db276011f302fc
Manolis Tzanidakis <mtzanidakis@gmail.com>
2022-02-09 02:55:57 +0200

    make max upload adjustable from env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,17 @@
 set -e
 umask 0022
 
+# usage: convert_units 32M (or 32G)
+# returns bytes
+conv_bytes() {
+	local _n=${1::-1}
+	case $1 in
+		*K) echo $(( ${_n} * 1024 )) ;;
+		*M) echo $(( ${_n} * 1024 * 1024 )) ;;
+		*G) echo $(( ${_n} * 1024 * 1024 * 1024 )) ;;
+	esac
+}
+
 prepare_unit() {
 	local _c=/var/lib/unit/conf.json
 	local _t=${_c}.template
@@ -10,11 +21,18 @@ prepare_unit() {
 	[[ -f ${_t} ]] || return 0
 
 	echo "initializing unit config"
+	local _postmax=${MAXUPLOAD:-32M}
+	local _uploadmax=${_postmax}
+	local _maxbody=$(conv_bytes ${_postmax})
+
 	local _max=${PHPMAX:-20}
 	local _spare=${PHPSPARE:-5}
 	local _mem=${PHPMEMORY:-256M}
 	local _exectime=${PHPEXECTIME:-60}
-	sed     -e "s:__PHPMAX__:${_max}:" \
+	sed     -e "s:__MAX_BODY_SIZE__:${_maxbody}:" \
+		-e "s:__POST_MAX_SIZE__:${_postmax}:" \
+		-e "s:__UPLOAD_MAX_FILESIZE__:${_uploadmax}:" \
+		-e "s:__PHPMAX__:${_max}:" \
 		-e "s:__PHPSPARE__:${_spare}:" \
 		-e "s:__PHPMEMORY__:${_mem}:" \
 		-e "s:__PHPEXECTIME__:${_exectime}:" \
